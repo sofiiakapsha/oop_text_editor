da8d991768283cfe6313ad26e85964314de4de81
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2026-06-29 15:24:02 +0300

    Added line header and text_editor.h with classes for different types of lines and class TextEditor with logic for main part

---
+++ oop_text_editor/text_editor.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include <string>
+#include <stack>
+#include "line.h"
+
+struct UndoRedoState {
+    std::vector<std::unique_ptr<Line>> linesSnapshot;
+};
+
+class Cursor {
+private: int indexLine, index;
+public:
+    Cursor(int l, int i) : indexLine(l), index(i) {}
+    ~Cursor() = default;
+
+    void RightAndLeft(bool side);
+    void UpAndDown(bool side);
+
+    bool PrintWithCursor();
+    void ReadingConsole();
+};
+
+class TextEditor {
+private:
+    std::vector<std::unique_ptr<Line>> lines;
+
+    std::stack<UndoRedoState> undoStack;
+    std::stack<UndoRedoState> redoStack;
+
+    void saveState();
+
+public:
+    TextEditor() = default;
+    ~TextEditor() = default;
+
+    Cursor cursor;
+
+    void newLine(std::unique_ptr<Line> newLine, bool isPrinting);
+    void Append();
+    void InsertPasteReplace(int choice);
+    void Search();
+    void DeleteAndCut(bool cut);
+    void Copy();
+    void printAll() const;
+
+    void Undo();
+    void Redo();
+
+    void Save(const std::string& filename);
+    void Load(const std::string& filename);
+};
